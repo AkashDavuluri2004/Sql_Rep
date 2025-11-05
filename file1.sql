a8e0329714b03ac7bf759936699da6ed1d5a8943
AkashDavuluri2004 <Davuluri.Akash@triconinfotech.com>
2025-11-05 14:04:41 +0530

    Create file1.sql

---
+++ file1.sql
@@ -0,0 +1,10 @@
+CREATE TABLE departments (
+    dept_id INT PRIMARY KEY,
+    dept_name VARCHAR(100)
+);
+
+CREATE TABLE employees (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(100),
+    dept_id INT
+);
